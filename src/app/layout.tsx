faf0b23690d0a9e87042d70c86c72e7b257e7df4
casperhol <casperholger@proton.me>
2026-03-18 09:45:00 +0100

    new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LiveChatScript from "./live-chat-script";
 
 
 const geistSans = Geist({
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <LiveChatScript />
       </body>
     </html>
   );
---
+++ src/app/live-chat-script.tsx
@@ -0,0 +1,9 @@
+import Script from "next/script";
+
+const LiveChatScript = () => {
+ return (
+    <Script src="//code.tidio.co/9cgzwkojaowsees4k3aaepkjfzdokapc.js"></Script>
+ )
+};
+
+export default LiveChatScript;
